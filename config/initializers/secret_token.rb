e3f446f605a5fcc0475ff800dc6e000ae730301d
Erin Walker <ewalker@ewalkerblog.co.za>
2013-04-15 22:08:08 +0200

    first_commit
    I just installed the app called alift. Still working on the name ;)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alift::Application.config.secret_token = '2e6b6b69b04e2e7c8434d1c84b38eb4c0d0fa2fd755bc4143dcca240c8ae3b6ef4ff4e1722c81b800e23798606f26add5513ed5a7311ecc096cb77204128d993'
